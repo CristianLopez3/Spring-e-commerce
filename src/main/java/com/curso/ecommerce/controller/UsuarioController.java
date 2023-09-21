b6cf86bb3ade5978b6f872a1aafc46ad78df12ed
Cristian-Lopez <126030346+CristianLopez3@users.noreply.github.com>
2023-09-20 20:39:33 -0500

    add web configure adapter

--- src/main/java/com/curso/ecommerce/controller/UsuarioController.java
+++ src/main/java/com/curso/ecommerce/controller/UsuarioController.java
@@ -10,6 +10,8 @@ import com.curso.ecommerce.service.IOrdenService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -29,6 +31,9 @@ public class UsuarioController {
 	@Autowired
 	private IUsuarioService usuarioService;
 
+
+	private PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+
 	// /usuario/registro
 
 	@Autowired
@@ -41,6 +46,7 @@ public class UsuarioController {
 	@PostMapping("/save")
 	public String save(Usuario usuario) {
 		logger.info("Usuario registro: {}", usuario);
+		usuario.setPassword(passwordEncoder.encode(usuario.getPassword()));
 		usuario.setTipo("USER");
 		usuarioService.save(usuario);		
 		return "redirect:/";
